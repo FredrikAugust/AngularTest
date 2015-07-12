fd029482bf1c5372a6ec4e5736a2f8f32ab44d35
Fredrik A. Madsen-Malmo <mail.fredrikaugust@gmail.com>
2015-07-13 01:45:47 +0200

    minor layouting, not sure what I'm doing

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 // Import libraries
 var express = require('express');
+var path = require('path');
 
 // Create app using express.js
 var app = express();
 
 // Home route
 app.get('/', function (req, res) {
-	res.render('src/index.html');
+	// Sends a static file to the server. Prepends the dir this file comes from
+	console.log('Serving file from: %s', path.join(__dirname + '/index.html'));
+	res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 // Set up server on port 3000
---
+++ src/js/angular/main.js
@@ -0,0 +1,6 @@
+// Declare a new Angular app
+var app = angular.module('testApp', []);
+
+app.controller('PostgresTesting', function () {
+
+});
