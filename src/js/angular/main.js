bef4c19820216208719c002b6208486876aa411b
Fredrik A. Madsen-Malmo <mail.fredrikaugust@gmail.com>
2015-07-13 02:51:01 +0200

    show each of the nodes from aether nodes

--- src/js/angular/main.js
+++ src/js/angular/main.js
@@ -2,5 +2,15 @@
 var app = angular.module('testApp', []);
 
 app.controller('PostgresController', ['$http', function ($http) {
-	this.nodes = [];
+	PostgresController = this;
+
+	$http.get('/pg')
+		.success(function (data) {
+			PostgresController.nodes = data;
+			console.log('Mr. Wizard succeeded!');
+		})
+		.error(function (err) {
+			console.error('Mr. Wizard could not make a post request to get the nodes.', err);
+			PostgresController.nodes = [];
+		});
 }]);
