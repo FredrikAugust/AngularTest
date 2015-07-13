c589ffc2a13e8b8f392d556551a7f9e209377e8a
Fredrik A. Madsen-Malmo <mail.fredrikaugust@gmail.com>
2015-07-13 18:32:18 +0200

    moved show-nodes to a directive and only show online nodes

--- src/js/angular/main.js
+++ src/js/angular/main.js
@@ -1,21 +1,29 @@
 // Declare a new Angular app
 var app = angular.module('testApp', []);
 
-app.controller('PostgresController', ['$http', function ($http) {
-	// Because if you refer to 'this' inside the $http.get it will assume
-	// you are referring to that instance of $http.somethingsomething
-	PostgresController = this;
+app.directive('showNodes', ['$http', function ($http) {
+	// Runs during compile
+	return {
+		controller: function ($http) {
+			// Because if you refer to 'this' inside the $http.get it will assume
+			// you are referring to that instance of $http.somethingsomething
+			PostgresController = this;
 
-	// Make a get request to /pg
-	$http.get('/pg')
-		.success(function (data) {
-			// this.nodes = data
-			PostgresController.nodes = data;
-			console.log('Nodes successfully retrieved');
-		})
-		.error(function (err) {
-			console.error('Could not get nodes', err);
-			// this.nodes = []
-			PostgresController.nodes = [];
-		});
+			// Make a get request to /pg
+			$http.get('/pg')
+				.success(function (data) {
+					// this.nodes = data
+					PostgresController.nodes = data;
+					console.log('Nodes successfully retrieved');
+				})
+				.error(function (err) {
+					console.error('Could not get nodes', err);
+					// this.nodes = []
+					PostgresController.nodes = [];
+				});
+		},
+		controllerAs: 'pgCtrl',
+		restrict: 'A',
+		templateUrl: 'partials/showNodes.html'
+	};
 }]);
