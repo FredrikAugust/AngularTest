3a8a84d7821d135159a71f80cdb19a78b9497bca
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2016-03-12 20:22:32 +0100

    indentation

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-// Import libraries
+// Import Libraries
 var express = require('express');
 var path = require('path');
 var pg = require('pg');
@@ -14,35 +14,35 @@ pg.defaults.ssl = true;
 
 // Home route
 app.get('/', function (req, res) {
-	// Sends a static file to the server. Prepends the dir this file comes from
-	console.log('Serving file from: %s', path.join(__dirname + '/index.html'));
-	res.sendFile(path.join(__dirname + '/index.html'));
+    // Sends a static file to the server. Prepends the dir this file comes from
+    console.log('Serving file from: %s', path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 // Trying to use postgres
 app.get('/pg', function (req, res) {
-	console.log('Connecting to database');
+    console.log('Connecting to database');
 
-	// Connect to the database, using the con string
-	pg.connect(pgCon, function (err, client, done) {
-		if (err) {
-			return console.error('Couldn\'t connect to database', err);
-		}
+    // Connect to the database, using the con string
+    pg.connect(pgCon, function (err, client, done) {
+	if (err) {
+	    return console.error('Couldn\'t connect to database', err);
+	}
 
-		console.log('Connection established, performing query');
+	console.log('Connection established, performing query');
 
-		// Exec query
-		client.query('select * from Entry', function (err, result) {
-			if (err) {
-				return console.error('Could not perform query', err);
-			};
+	// Exec query
+	client.query('select * from Entry', function (err, result) {
+	    if (err) {
+		return console.error('Could not perform query', err);
+	    };
 
-			console.log('Query completed successfully');
+	    console.log('Query completed successfully');
 
-			// Send a response
-			res.send(result.rows);
-		});
+	    // Send a response
+	    res.send(result.rows);
 	});
+    });
 });
 
 // Express middleware?
--- src/js/angular/main.js
+++ src/js/angular/main.js
@@ -2,28 +2,28 @@
 var app = angular.module('testApp', []);
 
 app.directive('showNodes', ['$http', function ($http) {
-	// Runs during compile
-	return {
-		controller: function ($http) {
-			// Because if you refer to 'this' inside the $http.get it will assume
-			// you are referring to that instance of $http.somethingsomething
-			PostgresController = this;
+    // Runs during compile
+    return {
+	controller: function ($http) {
+	    // Because if you refer to 'this' inside the $http.get it will assume
+	    // you are referring to that instance of $http.somethingsomething
+	    PostgresController = this;
 
-			// Make a get request to /pg
-			$http.get('/pg')
-				.success(function (data) {
-					// this.nodes = data
-					PostgresController.nodes = data;
-					console.log('Nodes successfully retrieved');
-				})
-				.error(function (err) {
-					console.error('Could not get nodes', err);
-					// this.nodes = []
-					PostgresController.nodes = [];
-				});
-		},
-		controllerAs: 'pgCtrl',
-		restrict: 'A',
-		templateUrl: 'partials/showNodes.html'
-	};
+	    // Make a get request to /pg
+	    $http.get('/pg')
+		.success(function (data) {
+		    // this.nodes = data
+		    PostgresController.nodes = data;
+		    console.log('Nodes successfully retrieved');
+		})
+		.error(function (err) {
+		    console.error('Could not get nodes', err);
+		    // this.nodes = []
+		    PostgresController.nodes = [];
+		});
+	},
+	controllerAs: 'pgCtrl',
+	restrict: 'A',
+	templateUrl: 'partials/showNodes.html'
+    };
 }]);
